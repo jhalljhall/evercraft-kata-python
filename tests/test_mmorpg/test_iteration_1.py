e76cc27434ecff69a23eb659394ee9032514c030
sflittrell <sflittrell@gmail.com>
2021-06-08 14:47:00 -0400

    first test

--- tests/test_mmorpg/test_iteration_1.py
+++ tests/test_mmorpg/test_iteration_1.py
@@ -1,4 +1,10 @@
 from foo.bar import bar
+from lib.models import Character
 
-def test_my_bar():
-    assert bar() == "foobar"
+# def test_my_bar():
+#     assert bar() == "foobar"
+
+def test_set_character_name():
+    c = Character
+    c.set_name('Bob Ross')
+    assert c.get_name() == 'Bob Ross'
