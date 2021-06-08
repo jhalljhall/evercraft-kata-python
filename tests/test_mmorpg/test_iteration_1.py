0539f3dec449314f8d39913d8b3c03354711a6f2
Rachel Ehlers <rachel.ehlers1@gmail.com>
2021-06-08 15:44:32 -0400

    Alignment tests

--- tests/test_mmorpg/test_iteration_1.py
+++ tests/test_mmorpg/test_iteration_1.py
@@ -1,4 +1,5 @@
 from lib.models.Character import Character
+from lib.utils.Alignment import Alignment
 
 def test_set_character_name():
     c = Character
@@ -9,7 +10,11 @@ def test_create_character():
     c = Character('Bob Ross')
     assert c.get_name() == 'Bob Ross'
 
-# def test_set_character_name():
-#     c = Character
-#     c.name = 'Bob Ross'
-#     assert c.name == 'Bob Ross'
+def test_set_alignment():
+    c = Character('Bob Ross')
+    c.set_alignment(Alignment.NEUTRAL)
+    assert c.alignment == Alignment.NEUTRAL
+
+def test_character_alignment():
+    c = Character('Bob Ross', Alignment.NEUTRAL)
+    assert c.alignment == Alignment.NEUTRAL
