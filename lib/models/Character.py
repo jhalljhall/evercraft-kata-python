c519b26c0a3e37b8f1b8f87f54d32661dcf4bc97
Rachel Ehlers <rachel.ehlers1@gmail.com>
2021-06-09 09:51:20 -0400

    Added armor class and hit class

--- lib/models/Character.py
+++ lib/models/Character.py
@@ -1,6 +1,9 @@
 from lib.utils.Alignment import Alignment
 
 class Character:
+    armor_class = 10
+    hit_points = 5
+
     def __init__(self, name, alignment = Alignment.NEUTRAL):
         self.name = name
         self.alignment = alignment
@@ -16,3 +19,11 @@ class Character:
     @classmethod
     def set_alignment(self, alignment):
         self.alignment = alignment
+
+    @classmethod
+    def get_armor_class(self):
+        return self.armor_class
+
+    @classmethod
+    def get_hit_points(self):
+        return self.hit_points
